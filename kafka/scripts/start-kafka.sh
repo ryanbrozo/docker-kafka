25f4a9604d18c0695eb4c497f214dea061317a4d
rye <ryanbrozo@gmail.com>
2016-08-23 08:39:37 +0800

    updated to kafka 0.10.0.0

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -15,14 +15,15 @@ if [ ! -z "$HELIOS_PORT_kafka" ]; then
 fi
 
 # Set the external host and port
-if [ ! -z "$ADVERTISED_HOST" ]; then
+if [ ! -z "$ADVERTISED_HOST" ] && [ ! -z "$ADVERTISED_PORT" ]; then
     echo "advertised host: $ADVERTISED_HOST"
-    sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
-fi
-if [ ! -z "$ADVERTISED_PORT" ]; then
     echo "advertised port: $ADVERTISED_PORT"
-    sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/#(advertised.listeners)=(.*)/\1=PLAINTEXT:\/\/$ADVERTISED_HOST:$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
 fi
+#if [ ! -z "$ADVERTISED_PORT" ]; then
+#    echo "advertised port: $ADVERTISED_PORT"
+#    sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+#fi
 
 # Set the zookeeper chroot
 if [ ! -z "$ZK_CHROOT" ]; then
